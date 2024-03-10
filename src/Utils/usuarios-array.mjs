e7aecc740e2ee7bc0c32e3b43a0ca5fff292549e
Freddy Antonio Ordonez Aguilar <freddy12aguilar@outlook.com>
2024-03-09 19:40:14 -0600

    Comienzo del proyecto

---
+++ src/Utils/usuarios-array.mjs
@@ -0,0 +1,20 @@
+export const usuarios = [
+  {
+    id: 1,
+    nombreCompleto: "Freddy Antonio Ordonez Aguilar",
+    genero: "M",
+    metodoPagos: [
+      {
+        id: 1,
+        tipoPago: "Paypal",
+        numeroTarjeta: "5678 5645 6767 2424",
+        idUsuario: 1,
+      },
+    ],
+    tipoPerfil: {
+      id: 1,
+      tipoPerfil: "Usuario",
+    },
+    contrasena: "12345"
+  },
+];
